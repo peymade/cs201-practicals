36c1f4cdc8024699c190a5faa9d9ea7c523f0fff
Peyton Mader <middpeyton@gmail.com>
2022-02-28 14:30:19 -0500

    Complete Song class practical

--- src/week2/Song.java
+++ src/week2/Song.java
@@ -6,26 +6,35 @@ public class Song {
   private String name;
   private String artist;
   private String genre;
-  private int minutes;
-  private int seconds;
+  private String minutes;
+  private String seconds;
   private String length;
 
-  public Song(String name, String artist, String genre, int minutes, int seconds) {
+  public Song(String name, String artist, String genre, String minutes, String seconds) {
     this.name = name;
     this.artist = artist;
     this.genre = genre;
     this.minutes = minutes;
     this.seconds = seconds;
 
-    this.length = minutes + ":" + seconds;
+    this.length = this.minutes + ":" + this.seconds;
+
   }
 
   public static void main(String[] args) {
 
-    Song Case = new Song("A Case Of You", "Joni Mitchell", "folk", 4, 24);
+    Song FIS = new Song("Feel It Still", "Portugal. The Man", "pop", "2", "44");
+    Song LYLT = new Song("Love You Long Time", "Pentatonix", "acapella", "3", "06");
+    Song SM = new Song("Sunday Morning", "The October Project", "pop", "4", "42");
+
+    System.out.println("These are songs that the Middlebury Mamajamas perform.");
+    System.out
+        .println(FIS.name + ", by " + FIS.artist + ", is a " + FIS.genre + " song. It is " + FIS.length + " long.");
 
     System.out
-        .println(Case.name + ", by " + Case.artist + ", is a " + Case.genre + " song. It is " + Case.length + " long.");
+        .println(LYLT.name + ", by " + LYLT.artist + ", is a " + LYLT.genre + " song. It is " + LYLT.length + " long.");
+
+    System.out.println(SM.name + ", by " + SM.artist + ", is a " + SM.genre + " song. It is " + SM.length + " long.");
 
   }
 }
