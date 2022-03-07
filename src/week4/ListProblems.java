42e2ffe8a34d095c50caa86ed857216ea729237e
Peyton Mader <middpeyton@gmail.com>
2022-03-07 14:27:08 -0500

    Completed List Problems

--- src/week4/ListProblems.java
+++ src/week4/ListProblems.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class is a collection of static methods.
- * You run it by going to {@link ListProblemsTest}.
+ * This class is a collection of static methods. You run it by going to
+ * {@link ListProblemsTest}.
  */
 public class ListProblems {
 
@@ -15,7 +15,15 @@ public class ListProblems {
      * @return a mutable list (no List.of for you)
      */
     public static List<String> makeABCD() {
-        throw new RuntimeException();
+        // throw new RuntimeException();
+
+        List<String> abcd = new ArrayList<>();
+        abcd.add("A");
+        abcd.add("B");
+        abcd.add("C");
+        abcd.add("D");
+
+        return abcd;
     }
 
     /**
@@ -25,7 +33,19 @@ public class ListProblems {
      * @return a new list containing the reversed input.
      */
     public static List<String> reverseList(List<String> input) {
-        throw new RuntimeException();
+        // throw new RuntimeException();
+
+        // loop through the list backwards, and add the number to the new list
+
+        List<String> reversed = new ArrayList<>();
+
+        for (int y = input.size(); y >= 1; y--) {
+            Integer arraySpot = y - 1;
+            reversed.add(input.get(arraySpot));
+        }
+
+        return reversed;
+
     }
 
     /**
@@ -35,7 +55,25 @@ public class ListProblems {
      * @return the position of the minimum value, e.g., 2.
      */
     public static int findMinimumPosition(List<Double> numbers) {
-        throw new RuntimeException();
+        // throw new RuntimeException();
+
+        Integer minValPos = -1;
+        Double checkingLowest;
+
+        if (numbers.size() > 0) {
+            minValPos = 0;
+            Double currentLowest = numbers.get(0);
+
+            for (int y = 1; y < numbers.size(); y++) {
+                checkingLowest = numbers.get(y);
+                if (checkingLowest < currentLowest) {
+                    currentLowest = checkingLowest;
+                    minValPos = y;
+                }
+            }
+        }
+
+        return minValPos;
     }
 
     /**
@@ -46,6 +84,15 @@ public class ListProblems {
      * @return a list with multiplied values, e.g., [2, 4, 6]
      */
     public static List<Integer> multiplyItems(List<Integer> numbers, int multiple) {
-        throw new RuntimeException();
+        // throw new RuntimeException();
+
+        List<Integer> multipliedVals = new ArrayList<>();
+
+        for (int y = 0; y < numbers.size(); y++) {
+            Integer multValue = (numbers.get(y) * multiple);
+            multipliedVals.add(multValue);
+        }
+
+        return multipliedVals;
     }
 }
